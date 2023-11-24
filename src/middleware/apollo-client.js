c8380cc943ca6135e8e1bc27eee4efbaa655f0ea
taylorhelene <taylorhelene09@gmail.com>
2023-11-24 15:58:51 +0300

    url

--- src/middleware/apollo-client.js
+++ src/middleware/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000",
+    uri: "https://mmm-pr5e.onrender.com",
     cache: new InMemoryCache(),
   });
 
