f471228cbc17bf8e54a39769282434d61e0244c3
taylorhelene <taylorhelene09@gmail.com>
2023-11-27 15:55:53 +0300

    images

--- src/home/Carousel.js
+++ src/home/Carousel.js
@@ -4,7 +4,7 @@ import styles from './../page.module.css';
 
 const Carousel = ()=>{
     const classImage="img-fluid opacity-100"
-    const arr = [<img className={classImage} src='/images/hulk.png'/>,<img className={classImage} src='/images/starwars.png'/>,<img className={classImage} src='/images/naruto.png'/>]
+    const arr = [<img className={classImage} src='https://raw.githubusercontent.com/taylorhelene/Comic-website-with-no-boilerplate/main/public/images/hulk.png'/>,<img className={classImage} src='https://raw.githubusercontent.com/taylorhelene/Comic-website-with-no-boilerplate/main/public/images/starwars.png'/>,<img className={classImage} src='https://raw.githubusercontent.com/taylorhelene/Comic-website-with-no-boilerplate/main/public/images/naruto.png'/>]
     let [imgArr,setImage]=React.useState(0);
 
     setTimeout(()=>{
--- src/utils/Header.js
+++ src/utils/Header.js
@@ -6,7 +6,7 @@ const Header = () =>{
     return(
         <div className='container mb-5 border-bottom rounded-bottom'>
         <header className='row'>
-        <img className='col-sm-2' src='/images/logo .png'/>
+        <img className='col-sm-2' src='https://raw.githubusercontent.com/taylorhelene/Comic-website-with-no-boilerplate/main/public/images/logo%20.png'/>
         <Navbar className='mx-auto' />
         </header>
         </div>
